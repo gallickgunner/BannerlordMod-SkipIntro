16a6c0a45010bb718b1fdb875b50b94a5c7705fb
gallickgunner <gallick-gunner@hotmail.com>
2022-02-03 01:20:35 +0500

    Add project files

---
+++ SkipIntro/ConfigFileManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+
+namespace SkipIntro
+{
+	internal static class ConfigFileManager
+	{
+		private static string assemblyDir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+		private static string parentDir = Directory.GetParent(Directory.GetParent(assemblyDir).ToString()).ToString();
+
+		private static string cfgPath = parentDir + "\\SkipIntro.cfg";
+		private static bool _skipMainIntro = true;
+		private static bool _skipSandboxIntro = true;
+		private static bool _quickStart = false;
+
+		public static bool SkipMainIntro
+        {
+			get { return _skipMainIntro; }
+        }
+
+		public static bool SkipSandboxIntro
+		{
+			get { return _skipSandboxIntro; }
+		}
+		public static bool QuickStart
+		{
+			get { return _quickStart; }
+		}
+
+		public static bool loadConfigFile(out string error)
+		{
+			bool err = false;
+			error = "";
+			if (File.Exists(cfgPath))
+			{				
+				foreach (string line in File.ReadLines(cfgPath))
+				{
+					if(!line.StartsWith("#") && !string.IsNullOrEmpty(line))
+					{
+						string[] option = line.Split(new char[] { '=' });
+						
+						if (option[0] == "skipMainIntro")
+							err = !Boolean.TryParse(option[1], out _skipMainIntro);
+						else if (option[0] == "skipCampaignIntro")
+							err = !Boolean.TryParse(option[1], out _skipSandboxIntro);
+						else if (option[0] == "skipCC")
+							err = !Boolean.TryParse(option[1], out _quickStart);
+
+						if (err)
+						{
+							error = "Error parsing options. Make sure there are no whitespaces and" +
+								" use 1 or 0 as values inside config file. Videos will be skipped by default.";
+							_skipMainIntro = true;
+							_skipSandboxIntro = true;
+							return false;
+						}
+					}
+				}					
+				return !err;
+			}
+			else
+			{
+				error = "Error finding the config file. Videos will be skipped by default.";
+				return err;
+			}
+		}
+	}
+}
---
+++ SkipIntro/SkipIntroSubModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using HarmonyLib;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace SkipIntro
+{
+    public class SkipIntroSubModule : MBSubModuleBase
+	{
+		private static string error = "";
+		protected override void OnSubModuleLoad()
+		{
+			base.OnSubModuleLoad();
+			ConfigFileManager.loadConfigFile(out error);
+			Harmony harmony = new Harmony("SkipIntro");
+			harmony.PatchAll();
+		}
+		protected override void OnBeforeInitialModuleScreenSetAsRoot()
+		{
+			if (!string.IsNullOrEmpty(error))
+				InformationManager.DisplayMessage(new InformationMessage(error));
+			base.OnBeforeInitialModuleScreenSetAsRoot();
+		}
+	}
+}
